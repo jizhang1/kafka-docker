8fa77c836afe4b66465063c32316fa49485579a9
Ji Zhang <ji.zhang@samsung.com>
2019-07-23 20:56:36 +0800

    Update the Dockerfile and start-kafka.sh

--- start-kafka.sh
+++ start-kafka.sh
@@ -135,4 +135,7 @@ if [[ -n "$CUSTOM_INIT_SCRIPT" ]] ; then
   eval $CUSTOM_INIT_SCRIPT
 fi
 
+# Append the Kafka StatsD Metrics properties
+cat /opt/kafka/config/ow.kafka.metrics.append.properties >> /opt/kafka/config/server.properties
+
 exec $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
